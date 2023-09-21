dd78807c952ce996d275da200e418ca94aabf47a
Luke Macauley <lukemacauley@mac.com>
2023-09-21 12:43:06 +0400

    squid test

--- src/env.mjs
+++ src/env.mjs
@@ -17,6 +17,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+    NEXT_PUBLIC_SQUID_ID: z.string().min(1),
   },
 
   /**
@@ -25,6 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_SQUID_ID: process.env.NEXT_PUBLIC_SQUID_ID,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
